989b0b73b8cd7025470e6fd56fa8b780fcc12f81
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-04-10 17:04:18 +0530

    Edit: Java Armstrong Number

--- Java/Math/Armstrong_Number/armstrongNumber.java
+++ Java/Math/Armstrong_Number/armstrongNumber.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
 
-//import java.util.*;
-
 public class armstrongNumber{
     private static Scanner scanner;
-    
+    //To find power
     public static int powers(int r, int s){
         int a = 1;
 
@@ -26,45 +24,30 @@ public class armstrongNumber{
         int temp = num; 
         int temp2 = num;
 
-    temp = num;
-    temp2 = num;
-    int remainder;
-    while(temp2!=0){
-        size+=1;
-        temp2 = temp2/10;
-    }
+        temp = num;
+        temp2 = num;
+        int remainder;
 
-    while(temp!=0){
-        remainder = temp%10;
-        sum += powers(remainder, size);
-        temp = temp/10;
-    }
+        //To find the number of digits
+        while(temp2!=0){
+            size+=1;
+            temp2 = temp2/10;
+        }
 
-    if(sum == num){
-        System.out.println(num+", It's an Armstrong number");
-    }
+        //calculating Armstrong Number
+        while(temp!=0){
+            remainder = temp%10;
+            sum += powers(remainder, size);
+            temp = temp/10;
+        }
+        
+        //Checking the given number is Armstrong Number
+        if(sum == num){
+            System.out.println(num+", It's an Armstrong number");
+        }
 
-    else{
-        System.out.println(num+ ", It's not an Armstrong number");
-    }
+        else{
+            System.out.println(num+ ", It's not an Armstrong number");
+        }
     }
 }
-
-/*num = int(input("Enter a number: ")) 
-
-sum = 0; size = 0; temp = num; temp2 = num
-
-while(temp2!=0):
-    size += 1
-    temp2 = int(temp2/10)
-
-while(temp!=0):
-    remainder = temp%10
-    sum += remainder**size
-    temp = int(temp/10)
-
-if(sum == num):
-    print("It's an armstrong number")
-else:
-    print("It's not an armstrong number")
-*/
