5610c473eecb23361512f39799f246d805a7b31a
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-04-28 15:22:11 +0530

    bug fix

--- Python/Data-Structure/circular-singly-linked-list/circular-singly-linked-list.py
+++ Python/Data-Structure/circular-singly-linked-list/circular-singly-linked-list.py
@@ -4,7 +4,7 @@ class Node:
         self.dataValue = dataValue
         self.next = None
 
-class singleLinkedList:
+class circularSingleLinkedList:
 
     def __init__(self):
         self.headValue = None
@@ -54,24 +54,38 @@ class singleLinkedList:
             if node.next is None:
                 return
             node = node.next
-            prev = node.next
+        prev = node.next
         node.next = Node(arg1)
         node = node.next
-        node.next = prev
-        while node.next != self.headValue:
-            node = node.next
-        node.next = self.headValue       
+        node.next = prev 
+        
+        #node = self.headValue
+        #for i in range(1,arg2-1):
+        #    node = node.next
+        #    prev = node.next
+        #node.next = Node(arg1)
+        #node = node.next
+        #node.next = prev
+        #while node.next != self.headValue:
+        #    node = node.next
+        #node.next = self.headValue       
             
     def delete(self, position: "Position to be deleted"):
         #[data|next] --> [data|next] --> [data|next] --> [data|next]
         #                        ^_______________^
-        node = self.headValue
-        for i in range(position-2):
-            node = node.next 
-        node.next = node.next.next
-        while node.next != self.headValue:
-            node = node.next
-        node.next = self.headValue
+        if position == 1:
+            node = self.headValue
+            while node.next != self.headValue:
+                node = node.next
+            node.next = self.headValue.next
+        else:
+            node = self.headValue
+            for i in range(position-2):
+                node = node.next 
+            node.next = node.next.next
+            while node.next != self.headValue:
+                node = node.next
+            node.next = self.headValue
 
     def display(self):
 
@@ -84,12 +98,13 @@ class singleLinkedList:
             print (printValue.dataValue)
             printValue = printValue.next
         pass
+    
     def hint(self):
         pass
 #creating object 
-#list = singleLinkedList()
+list = circularSingleLinkedList()
 
-#list.insertLast(50, 60,70)
+list.insertLast(50, 60,70)
 #list.display()
 
 '''
@@ -104,7 +119,7 @@ output:
 50...
 '''
 
-#list.insertFirst(10,20,30)
+list.insertFirst(10,20,30)
 #list.display()
 
 '''
@@ -123,7 +138,7 @@ output:
 '''
 
 #print(list.insertMiddle.__annotations__)
-#list.insertMiddle(40,4)
+list.insertMiddle(40,4)
 #list.display()
 '''
 It shows the inserted element at nth position 
@@ -141,7 +156,7 @@ output:
 10...
 '''
 
-#list.delete(6)
+list.delete(1)
 #list.display()
 '''
 It shows the list after deleting it 
